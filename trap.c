719aaad4f50c5bfc10c4013e0d568173089827b6
rohanbehera <rohanbehera03@gmail.com>
2022-06-05 16:21:58 -0700

    Changed for loop traversal condition

--- trap.c
+++ trap.c
@@ -19,117 +19,117 @@ uint fault; //initialize fault
 void
 tvinit(void)
 {
-  int i;
+    int i;
 
-  for(i = 0; i < 256; i++)
+    for(i = 0; i < 256; i++)
     SETGATE(idt[i], 0, SEG_KCODE<<3, vectors[i], 0);
-  SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
+    SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
 
-  initlock(&tickslock, "time");
+    initlock(&tickslock, "time");
 }
 
 void
 idtinit(void)
 {
-  lidt(idt, sizeof(idt));
+    lidt(idt, sizeof(idt));
 }
 
 //PAGEBREAK: 41
 void
 trap(struct trapframe *tf)
 {
-  if(tf->trapno == T_SYSCALL){
-    if(myproc()->killed)
-      exit();
-    myproc()->tf = tf;
-    syscall();
-    if(myproc()->killed)
-      exit();
-    return;
-  }
-
-  switch(tf->trapno){
-  case T_IRQ0 + IRQ_TIMER:
-    if(cpuid() == 0){
-      acquire(&tickslock);
-      ticks++;
-      wakeup(&ticks);
-      release(&tickslock);
+    if(tf->trapno == T_SYSCALL){
+        if(myproc()->killed)
+            exit();
+        myproc()->tf = tf;
+        syscall();
+        if(myproc()->killed)
+            exit();
+        return;
     }
-    lapiceoi();
-    break;
-  case T_IRQ0 + IRQ_IDE:
-    ideintr();
-    lapiceoi();
-    break;
-  case T_IRQ0 + IRQ_IDE+1:
-    // Bochs generates spurious IDE1 interrupts.
-    break;
-  case T_IRQ0 + IRQ_KBD:
-    kbdintr();
-    lapiceoi();
-    break;
-  case T_IRQ0 + IRQ_COM1:
-    uartintr();
-    lapiceoi();
-    break;
-  case T_IRQ0 + 7:
-  case T_IRQ0 + IRQ_SPURIOUS:
-    cprintf("cpu%d: spurious interrupt at %x:%x\n",
-            cpuid(), tf->cs, tf->eip);
-    lapiceoi();
-    break;
+
+    switch(tf->trapno){
+        case T_IRQ0 + IRQ_TIMER:
+            if(cpuid() == 0){
+                acquire(&tickslock);
+                ticks++;
+                wakeup(&ticks);
+                release(&tickslock);
+            }
+            lapiceoi();
+            break;
+        case T_IRQ0 + IRQ_IDE:
+            ideintr();
+            lapiceoi();
+            break;
+        case T_IRQ0 + IRQ_IDE+1:
+            // Bochs generates spurious IDE1 interrupts.
+            break;
+        case T_IRQ0 + IRQ_KBD:
+            kbdintr();
+            lapiceoi();
+            break;
+        case T_IRQ0 + IRQ_COM1:
+            uartintr();
+            lapiceoi();
+            break;
+        case T_IRQ0 + 7:
+        case T_IRQ0 + IRQ_SPURIOUS:
+            cprintf("cpu%d: spurious interrupt at %x:%x\n",
+                    cpuid(), tf->cs, tf->eip);
+            lapiceoi();
+            break;
 
 
 //Lab 3 Part 2
-case T_PGFLT:
-          curproc = myproc(); //set current process to myprocess
-          fault = rcr2(); //look up page table entry for faulting address, check if its present but not writable
-          if (fault > CURRTOPSTACK) {
-              exit();
-          }
-          //  go to the default handler and do a kernel panic as before
-          if (allocuvm(curproc->pgdir, PGROUNDDOWN(fault), fault) == 0) {
-              cprintf("Page fault, Allocuvm failed, Current number of pages: %d\n", curproc->stackSize);
-              exit();
-          }
-        //  check if the page fault is caused by an access to the pageguard (the page under
-        //  current top of stack);
-          else {
-              curproc->stackSize = curproc->stackSize + 1; //allocate and map the page
-              cprintf("Increased stack size\n\n", curproc->stackSize, fault);
-          }
-          break;
+        case T_PGFLT:
+            curproc = myproc(); //set current process to myprocess
+            fault = rcr2(); //look up page table entry for faulting address, check if its present but not writable
+            if (fault > CURRTOPSTACK) {
+                exit();
+            }
+            //  go to the default handler and do a kernel panic as before
+            if (allocuvm(curproc->pgdir, PGROUNDDOWN(fault), fault) == 0) {
+                cprintf("Page fault, Allocuvm failed, Current number of pages: %d\n", curproc->stackSize);
+                exit();
+            }
+                //  check if the page fault is caused by an access to the pageguard (the page under
+                //  current top of stack);
+            else {
+                curproc->stackSize = curproc->stackSize + 1; //allocate and map the page
+                cprintf("Increased stack size\n\n", curproc->stackSize, fault);
+            }
+            break;
 
-  //PAGEBREAK: 13
-  default:
-    if(myproc() == 0 || (tf->cs&3) == 0){
-      // In kernel, it must be our mistake.
-      cprintf("unexpected trap %d from cpu %d eip %x (cr2=0x%x)\n",
-              tf->trapno, cpuid(), tf->eip, rcr2());
-      panic("trap");
+            //PAGEBREAK: 13
+        default:
+            if(myproc() == 0 || (tf->cs&3) == 0){
+                // In kernel, it must be our mistake.
+                cprintf("unexpected trap %d from cpu %d eip %x (cr2=0x%x)\n",
+                        tf->trapno, cpuid(), tf->eip, rcr2());
+                panic("trap");
+            }
+            // In user space, assume process misbehaved.
+            cprintf("pid %d %s: trap %d err %d on cpu %d "
+                    "eip 0x%x addr 0x%x--kill proc\n",
+                    myproc()->pid, myproc()->name, tf->trapno,
+                    tf->err, cpuid(), tf->eip, rcr2());
+            myproc()->killed = 1;
     }
-    // In user space, assume process misbehaved.
-    cprintf("pid %d %s: trap %d err %d on cpu %d "
-            "eip 0x%x addr 0x%x--kill proc\n",
-            myproc()->pid, myproc()->name, tf->trapno,
-            tf->err, cpuid(), tf->eip, rcr2());
-    myproc()->killed = 1;
-  }
 
-  // Force process exit if it has been killed and is in user space.
-  // (If it is still executing in the kernel, let it keep running
-  // until it gets to the regular system call return.)
-  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
-    exit();
+    // Force process exit if it has been killed and is in user space.
+    // (If it is still executing in the kernel, let it keep running
+    // until it gets to the regular system call return.)
+    if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
+        exit();
 
-  // Force process to give up CPU on clock tick.
-  // If interrupts were on while locks held, would need to check nlock.
-  if(myproc() && myproc()->state == RUNNING &&
-     tf->trapno == T_IRQ0+IRQ_TIMER)
-    yield();
+    // Force process to give up CPU on clock tick.
+    // If interrupts were on while locks held, would need to check nlock.
+    if(myproc() && myproc()->state == RUNNING &&
+       tf->trapno == T_IRQ0+IRQ_TIMER)
+        yield();
 
-  // Check if the process has been killed since we yielded
-  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
-    exit();
+    // Check if the process has been killed since we yielded
+    if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
+        exit();
 }
